a94ba4adfcb992626e4682f7bf49acc441722090
Roy <rreynes@nerubia.com>
2016-11-16 22:26:54 +0800

    initial commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Product::class, 10)->create();
+        factory(App\Product::class, 5)->create();
     }
 }
